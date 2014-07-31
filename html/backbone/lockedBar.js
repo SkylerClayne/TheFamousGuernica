f625dff92601f546ac7b8b7c74e240fe91d20ecf
Skyler Layne <SkylerClayne@gmail.com>
2014-07-31 16:16:04 -0400

    commit

--- html/backbone/lockedBar.js
+++ html/backbone/lockedBar.js
@@ -2,14 +2,18 @@
   		window.addEventListener("scroll",navbar_reset_top,false);
 	}
 
-	var navbar_top=100;
+	var navbar_top=125;
+  var cssRuleCode = document.all ? 'rules' : 'cssRules';
+  // for loop decreasing the top-margin as scroll increases.
 
 function navbar_reset_top() {
   var scrollTop=document.documentElement.scrollTop||document.body.scrollTop;
   if(scrollTop>navbar_top&&navbar.className==="navbar_absolute") {
     document.getElementById("navbar").className="navbar_fixed";
-  }
+    //document.getElementById("submenu").className="submenu_fixed"
+  } 
   else if(scrollTop<navbar_top&&navbar.className==="navbar_fixed") {
     document.getElementById("navbar").className="navbar_absolute";
+    //document.getElementById("submenu").className="submenu_absolute";
   }
 }
